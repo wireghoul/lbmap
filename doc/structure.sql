1a49170041d7b83c7f5cb71363fac42ab1da794b
Eldar Marcussen <wireghoul@justanotherhacker.com>
2012-03-22 20:04:44 -0400

    Removing first set of test tools

--- doc/structure.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE server (id integer primary key autoincrement, hostname text, port text, has_lb integer, has_waf integer, server text, lb text, waf text, use_ssl integer);
-CREATE TABLE request (id integer primary key autoincrement, request text not null, notes text);
-CREATE TABLE response (server_id integer, request_id integer, response text not null, time text not null, http_version text, code text);
