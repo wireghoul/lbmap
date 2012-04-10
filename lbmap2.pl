e39dd85cead04ec43d29e502f0fe9dcd2fa153b0
Eldar Marcussen <wireghoul@justanotherhacker.com>
2012-04-10 02:52:42 -0400

    Signature output

--- lbmap2.pl
+++ lbmap2.pl
@@ -16,7 +16,7 @@ my %result = $lbmap->scan($ARGV[0]);
 print "Loadbalancer: $result{'loadbalancer'}\n";
 print "WAF: $result{'WAF'}\n";
 print "Backends: ".join "\n", keys(%{ $result{'backends'} });
-print "\n";
+print "\nSignature: $result{'signature'}\n";
 
 sub show_help {
     print "Usage $0 http://somehost/\n";
