27967fe97652344d008035cd7a4e7e29e67302b5
Eldar Marcussen <wireghoul@justanotherhacker.com>
2012-04-12 04:04:30 -0400

    Added passive detection of server headers

---
+++ lib/lbmap/Passive/Server.pm
@@ -0,0 +1,42 @@
+package lbmap::Passive::Server;
+
+use strict;
+use warnings;
+use lbmap::lbmap;
+
+=head1 NAME
+
+lbmap::Passive::Server - Detects web server headers
+
+=head1 VERSION
+
+Version 0.1
+
+=cut
+
+# Globals
+our $VERSION = '0.1';
+our $AUTHOR = 'Eldar Marcussen - http://www.justanotherhacker.com';
+
+=head1 DESCRIPTION
+lbmap::Passive::Server detects server headers
+=cut
+
+sub new {
+    my ($class, $parent) = @_;
+    my $self = {};
+    $self->{'parent'} = $parent;
+    bless $self, $class;
+    $self->{'parent'}->add_passive_detect('Server_header', 'Server: .*', \&detect_via_header );
+    return $self;
+}
+
+
+sub detect_via_header {
+    my ($parent, $http_response) = @_;
+    if ($http_response =~ m/Server: (.*)\r\n/o) {
+        $parent->add_result('webserver', $1);
+    }
+}
+
+1;
