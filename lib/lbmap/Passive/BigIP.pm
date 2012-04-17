9a3c6a4a279a86446c57ffb93c3f681fce40a9ec
Eldar Marcussen <wireghoul@justanotherhacker.com>
2012-04-17 19:05:10 -0400

    Now with thrice the bigip decoding functionality

--- lib/lbmap/Passive/BigIP.pm
+++ lib/lbmap/Passive/BigIP.pm
@@ -28,6 +28,8 @@ sub new {
     $self->{'parent'} = $parent;
     bless $self, $class;
     $self->{'parent'}->add_passive_detect('BIGipv4', 'Set-Cookie: BIGip.*=\d+\.\d+\.\d+', \&decode_bigipv4 );
+    $self->{'parent'}->add_passive_detect('BIGipv6', 'Set-Cookie: BIGip.*=vi.*', \&decode_bigipv6 );
+    $self->{'parent'}->add_passive_detect('BIGiprd', 'Set-Cookie: BIGip.*=rd.+o00000000000000000000ffff.+o.+', \&decode_bigiproutedomain );
     return $self;
 }
 
@@ -39,6 +41,26 @@ sub decode_bigipv4 {
         my $backend = join ".", map {hex} reverse ((sprintf "%08x", $host) =~ /../g);
         $backend.=":".hex join "", reverse((sprintf "%02x", $port) =~ /../g);
         $parent->add_result('backend',$backend);
+        $parent->add_result('loadbalancer', 'BIGIP');
+    }
+}
+
+sub decode_bigipipv6 {
+    my ($parent, $http_response) = @_;
+    if ($http_response =~ m/(BIGip.*)=vi(.{4})(.{4})(.{4})(.{4})(.{4})(.{4})\.(.+)/) {
+        my ($pool, $ipv6, $port) = ($1, "$2:$3:$4:$5:$6:$7", $8);
+        my $backend = "$ipv6:$port";
+	$parent->add_result('backend', $backend);
+        $parent->add_result('loadbalancer', 'BIGIP');
+    }
+}
+
+sub decode_bigiproutedomain {
+    my ($parent, $http_response) = @_;
+    if ($http_response =~ m/(BIGip.*)=rd(.+)o00000000000000000000ffff(..)(..)(..)(..)o(.+)/) {
+        my ($pool, $route_id, $ip1, $ip2, $ip3, $ip4, $port) = ($1, $2, $3, $4, $5, $6, $7);
+        my $backend = hex($ip1).".".hex($ip2).".".hex($ip3).".".hex($ip4).":$port";
+        $parent->add_result('backend', $backend);
         $parent->add_result('loadbalancer', 'BIGIP');
     }
 }
